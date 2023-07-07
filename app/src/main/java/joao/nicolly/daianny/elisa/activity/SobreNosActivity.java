f40b72f023ec62fdb7274b326463b02f180cfccd
elian <elisaaj.es@gmail.com>
2023-07-07 05:15:20 -0300

    Criei activity sobre_nos

---
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/SobreNosActivity.java
@@ -0,0 +1,15 @@
+package joao.nicolly.daianny.elisa.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import joao.nicolly.daianny.elisa.R;
+
+public class SobreNosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre_nos);
+    }
+}

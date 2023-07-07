0e17e0eebd15691ad9a1c3d813d85db9c1d36f2e
20201TIIMI0225 <elisaaj.es@gmail.com>
2023-07-07 10:50:41 -0300

    MUITAS ALTERAÇÕES

---
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/PreparosActivity.java
@@ -0,0 +1,16 @@
+package joao.nicolly.daianny.elisa.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import joao.nicolly.daianny.elisa.R;
+
+public class PreparosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preparos);
+    }
+}

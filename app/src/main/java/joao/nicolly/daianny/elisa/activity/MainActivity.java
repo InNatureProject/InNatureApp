99d53dd724e983f457d02ff3c07752bd203fb912
20201TIIMI0225 <elisaaj.es@gmail.com>
2023-09-12 09:25:23 -0300

    Acertando bugs layout

--- app/src/main/java/joao/nicolly/daianny/elisa/activity/MainActivity.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/MainActivity.java
@@ -1,16 +1,36 @@
 package joao.nicolly.daianny.elisa.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import joao.nicolly.daianny.elisa.R;
 
 public class MainActivity extends AppCompatActivity {
+    //Variáveis
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.btNav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.favViewOp:
+
+                }
+                return false;
+            }
+        });
     }
 }

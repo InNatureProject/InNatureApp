f3462d5126b3286fe78d06fc5de17a8ff105f5d5
elian <elisaaj.es@gmail.com>
2023-09-20 17:52:09 -0300

    Preparo arrumado

---
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/ReceitaPreparoActivity.java
@@ -0,0 +1,16 @@
+package joao.nicolly.daianny.elisa.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import joao.nicolly.daianny.elisa.R;
+
+public class ReceitaPreparoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receita_preparo);
+    }
+}

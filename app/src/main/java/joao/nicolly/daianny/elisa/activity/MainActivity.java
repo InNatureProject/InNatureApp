6ef501e9662023db612fff3b10ad7a8d24ee5059
elisa_jesus <elisaaj.es@gmail.com>
2023-09-29 13:19:04 -0300

    activity_receita_preparo.xml acertada

--- app/src/main/java/joao/nicolly/daianny/elisa/activity/MainActivity.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -23,22 +24,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //pegamdo modelo de visualização
-        final MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-
         //Mudando com qual item selecionado a activity deve iniciar
         bottomNavigationView = findViewById(R.id.btNav);
 //        bottomNavigationView.setSelectedItemId(R.id.homeViewOp);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                mainViewModel.setNavigationOpSelected(item.getItemId());
                 switch (item.getItemId()){
                     case R.id.favViewOp:
+                        toolbar.setBackgroundColor(000000);
                         break;
                     case R.id.homeViewOp:
+                        toolbar.setBackgroundColor(111111);
                         break;
                     case R.id.perfilViewOp:
+                        toolbar.setBackgroundColor(444444);
                         break;
 
                 }
--- app/src/main/java/joao/nicolly/daianny/elisa/auxiliares/MainViewModel.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/auxiliares/MainViewModel.java
@@ -1,6 +1,14 @@
 package joao.nicolly.daianny.elisa.auxiliares;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
 public class MainViewModel {
+
+    public MainViewModel(@NonNull Application application){
+        super();
+    }
     public void setNavigationOpSelected(int itemId) {
     }
 }

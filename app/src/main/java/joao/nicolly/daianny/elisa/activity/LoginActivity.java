c14d09f7ada1541eb7df1e91e5f486ffc8b8788b
elisa_jesus <elisaaj.es@gmail.com>
2023-10-06 14:15:00 -0300

    Criado LoginViewModel.java

--- app/src/main/java/joao/nicolly/daianny/elisa/activity/LoginActivity.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/activity/LoginActivity.java
@@ -1,12 +1,12 @@
 package joao.nicolly.daianny.elisa.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import joao.nicolly.daianny.elisa.R;
+import joao.nicolly.daianny.elisa.model.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+    LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

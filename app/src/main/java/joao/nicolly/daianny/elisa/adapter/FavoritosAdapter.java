8d0dcc158db3bc52eca8bee89dd3ad5d0b17c2e1
elisa_jesus <elisaaj.es@gmail.com>
2023-10-11 20:46:32 -0300

    Implementada a biblioteca paging

--- app/src/main/java/joao/nicolly/daianny/elisa/adapter/FavoritosAdapter.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/adapter/FavoritosAdapter.java
@@ -1,7 +1,27 @@
 package joao.nicolly.daianny.elisa.adapter;
 
+import android.view.ViewGroup;
+
 import joao.nicolly.daianny.elisa.model.Planta;
+import kotlinx.coroutines.CoroutineDispatcher;
+
+import androidx.annotation.NonNull;
 import androidx.paging.PagingDataAdapter;
+import androidx.recyclerview.widget.DiffUtil;
 
 public class FavoritosAdapter extends PagingDataAdapter<Planta,MyViewHolder>{
+    public FavoritosAdapter(@NonNull DiffUtil.ItemCallback<Planta> diffCallback, @NonNull CoroutineDispatcher mainDispatcher) {
+        super(diffCallback, mainDispatcher);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
 }

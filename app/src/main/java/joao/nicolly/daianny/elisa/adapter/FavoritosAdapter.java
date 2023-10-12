80d3dfbd85d7ddcce613104dd8bbab28bb0d4275
elisa_jesus <elisaaj.es@gmail.com>
2023-10-12 06:59:00 -0300

    FavoritosAdapter feito

--- app/src/main/java/joao/nicolly/daianny/elisa/adapter/FavoritosAdapter.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/adapter/FavoritosAdapter.java
@@ -1,7 +1,13 @@
 package joao.nicolly.daianny.elisa.adapter;
 
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import joao.nicolly.daianny.elisa.R;
 import joao.nicolly.daianny.elisa.model.Planta;
 import kotlinx.coroutines.CoroutineDispatcher;
 
@@ -17,11 +23,31 @@ public class FavoritosAdapter extends PagingDataAdapter<Planta,MyViewHolder>{
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_planta,parent,false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        // Como se fosse para cada planta
+        Planta planta = getItem(position);
+
+        //Aqui pegaremos a informação necessárias da planta e a dentro dos devidos elementos do item_planta;
+
+        TextView tvNome = holder.itemView.findViewById(R.id.tvNome);
+        tvNome.setText(planta.getNome());
+
+        TextView tvNomeCientifico = holder.itemView.findViewById(R.id.tvNoomeCientifico);
+        tvNomeCientifico.setText(planta.getNome_cientifico());
+
+        TextView tvDescricaoPlanta = holder.itemView.findViewById(R.id.tvDescricaoPlanta);
+        tvDescricaoPlanta.setText(planta.getInformacao());
+
+        ImageView imgvImagemPlanta = holder.itemView.findViewById(R.id.imgvImagemPlanta);
+        imgvImagemPlanta.setImageBitmap(planta.getImagem());
+
 
     }
 }
--- app/src/main/java/joao/nicolly/daianny/elisa/model/Planta.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/model/Planta.java
@@ -15,10 +15,34 @@ public class Planta {
 
     public Planta(String nome, String nomeCientifico, Bitmap imagem, String informacao, ArrayList<Preparo> preparos, ArrayList<Comentario> comentarios) {
         this.nome = nome;
-        nome_cientifico = nomeCientifico;
+        this.nome_cientifico = nomeCientifico;
         this.imagem = imagem;
         this.informacao = informacao;
         this.preparos = preparos;
         this.comentarios = comentarios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNome_cientifico() {
+        return nome_cientifico;
+    }
+
+    public Bitmap getImagem() {
+        return imagem;
+    }
+
+    public String getInformacao() {
+        return informacao;
+    }
+
+    public ArrayList<Preparo> getPreparos() {
+        return preparos;
+    }
+
+    public ArrayList<Comentario> getComentarios() {
+        return comentarios;
     }
 }

e7e0654cd798fb6283c915f7422cf422e2e27632
elisa_jesus <elisaaj.es@gmail.com>
2023-10-31 09:09:06 -0300

    acertos em LoginViewModel

--- app/src/main/java/joao/nicolly/daianny/elisa/model/LoginViewModel.java
+++ app/src/main/java/joao/nicolly/daianny/elisa/model/LoginViewModel.java
@@ -5,8 +5,10 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LoginViewModel extends AndroidViewModel {
     public LoginViewModel(@NonNull Application application) {
@@ -16,12 +18,14 @@ public class LoginViewModel extends AndroidViewModel {
     //Método que cria e executa uma reuisição para o servidor
 
     public LiveData<Boolean> login(String email, String senha ){
-        MutableLiveData<Boolean> result= new MultableLiveData<>();
-        ExecutorService.execute(new Runnable() {
+        MutableLiveData<Boolean> result= new MutableLiveData<>();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
             @Override
             public void run() {
-                ProductsRepository productsRepository= new ProductsRepository(getApplication());
-                boolean b= productsRepository.login(email,senha);
+                InNatureRepository inNatureRepository= new  InNatureRepository(getApplication());
+                boolean b = inNatureRepository.login(email,senha);
+                //TODO:é necessário criar o método login em InNatureRepository
                 result.postValue(b);
             }
         });
